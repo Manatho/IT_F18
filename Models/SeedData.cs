a58db724d9ad98cb98dbddc18d66003a6bab2a8a
Unknown <mathies-h@hotmail.com>
2018-05-15 10:08:20 +0200

    Added data seeder

---
+++ Models/SeedData.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IT_F18.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BlogDB(
+                serviceProvider.GetRequiredService<DbContextOptions<BlogDB>>()))
+            {
+                // Look for any movies.
+                if (!context.AboutInfo.Any())
+                {
+                    context.AboutInfo.Add(
+                     new AboutInfo
+                     {
+                         BirthDay = new DateTime(1995, 10, 14),
+                         CurrentOccupation = "Student",
+                         Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit ",
+                         Name = "Mathies F Hovedskou",
+                     });
+                }
+
+                if (!context.GalleryEntry.Any())
+                {
+                    context.GalleryEntry.AddRange(
+                        new GalleryEntry
+                        {
+                            Description = "pink pink pink",
+                            Title = "Pink",
+                            ImagePath = "/Pink.png"
+                        },
+                        new GalleryEntry
+                        {
+                            Description = "green green green",
+                            Title = "Green",
+                            ImagePath = "/Green.png"
+                        }
+
+                        );
+                }
+
+                if (!context.Subscriber.Any())
+                {
+                    context.Subscriber.Add(
+                        new Subscriber
+                        {
+                            Email = "asd@asd.com"
+                        });
+                }
+
+
+
+                
+
+               
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using IT_F18.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace IT_F18
@@ -14,7 +17,28 @@ namespace IT_F18
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<BlogDB>();
+                    // requires using Microsoft.EntityFrameworkCore;
+                    context.Database.Migrate();
+                    // Requires using RazorPagesMovie.Models;
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
